eba3057958c8f6d61a11f7376cd4deb2796cef5a
murugeswari1811 <murugeswari@vinkweb.com>
2024-04-29 14:22:08 +0530

    Image Gallery

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Home from './components/home.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>
-  },
-  {
-    path: "/app",
-    element: <App/>,
-  },
-]);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   Link,
+// } from "react-router-dom";
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home/>
+//   },
+//   {
+//     path: "/app",
+//     element: <App/>,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  // <RouterProvider router={router} />
+  <Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/app" element={<App />} />
+  </Routes>
+  </Router>
 )
